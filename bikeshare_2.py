e73d842dd452d8c5c02371366aafa3f993bf812e
Tamar <mischegoss@gmail.com>
2020-03-26 00:28:35 -0400

    Refactor problem set one

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -18,8 +18,7 @@ def get_filters():
     """
 
     print('\nHello! Let\'s explore some US bikeshare data!')
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
+    
 
     while True:
       city = input("\nWhich city would you like to filter by? New York City, Chicago or Washington?\n")
@@ -28,8 +27,6 @@ def get_filters():
         continue
       else:
         break
-
-    # TO DO: get user input for month (all, january, february, ... , june)
 
     while True:
       month = input("\nWhich month would you like to filter by? January, February, March, April, May, June or type 'all' if you do not have any preference?\n")
